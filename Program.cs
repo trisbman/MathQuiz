eb075c2e587e102b8fdd474af3817aa4bddf4247
Tristan Budiman <29161072+trisbman@users.noreply.github.com>
2022-04-15 14:06:03 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathQuiz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int a = rnd.Next(100);
+            int b = rnd.Next(100);
+            Console.WriteLine(a + " + " + b);
+            int result = a + b;
+
+            Console.Write("Answer: ");
+            string input = Console.ReadLine();
+
+            Console.WriteLine(result == int.Parse(input));
+
+            // pause
+            Console.ReadKey();
+        }
+    }
+}
